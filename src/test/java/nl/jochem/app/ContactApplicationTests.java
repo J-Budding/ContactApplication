84ea1717a53878642199a5b82c3461299c8b70ea
Jochem Budding <jochem.budding@mn.nl>
2021-03-25 00:30:04 +0100

    naamgeving issue test package en naam in pom.xml

--- src/test/java/nl/jochem/app/ContactApplicationTests.java
+++ src/test/java/nl/jochem/app/ContactApplicationTests.java
@@ -1,4 +1,4 @@
-package nl.xjb.app;
+package nl.jochem.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/nl/jochem/app/ContactControllerTest.java
@@ -0,0 +1,21 @@
+package nl.jochem.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ContactControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testListContact() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+		
+	}
+
+}
